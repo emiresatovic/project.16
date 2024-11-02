8853020655d857216a034e43bef6da6dd4bdf9c1
Emir Esatović <165719085+emiresatovic@users.noreply.github.com>
2024-11-02 09:56:47 +0100

    02.11 9h

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,15 @@
+import Button from './UI/Button.jsx';
+import logoImg from '../assets/logo.jpg';
+
 export default function Header() {
     return (
-        <header>
-            <div>
-                <img />
-                <h1></h1>
+        <header id="main-header">
+            <div id="title">
+                <img src={logoImg} alt='a restoraunt' />
+                <h1>AlminaFood</h1>
             </div>
             <nav>
-                <button>Cart (0)</button>
+                <Button textOnly>Cart (0)</Button>
             </nav>
         </header>
     )
